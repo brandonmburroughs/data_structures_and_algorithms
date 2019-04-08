bb850d8926209204ff2aef250fde68c5ddba382e
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2019-04-07 18:38:47 -0700

    Adding solution

--- 0_algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ 0_algorithmic_toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,15 +1,29 @@
 # Uses python3
 import sys
 
+
 def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+    low, high = 0, len(a)
+
+    while low < high:
+        mid = low + (high - low) // 2
+
+        if a[mid] == x:
+            return mid
+        elif x < a[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return -1
+
 
 def linear_search(a, x):
     for i in range(len(a)):
         if a[i] == x:
             return i
     return -1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -19,4 +33,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
