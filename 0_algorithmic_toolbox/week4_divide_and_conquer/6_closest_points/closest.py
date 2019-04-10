27a2c5d49b0e574beb190fcdbfc8088c6122da6d
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2019-04-09 20:45:29 -0700

    Presorting points

--- 0_algorithmic_toolbox/week4_divide_and_conquer/6_closest_points/closest.py
+++ 0_algorithmic_toolbox/week4_divide_and_conquer/6_closest_points/closest.py
@@ -19,7 +19,7 @@ def minimum_distance_brute_force(x, y):
     return min_distance
 
 
-def minimum_distance(x, y):
+def minimum_distance_too_slow(x, y):
     # Handle base cases for small groups of points
     if len(x) == 1:
         return sys.maxsize
@@ -60,8 +60,8 @@ def minimum_distance(x, y):
         s1_y.append(s2_y.pop(idx))
 
     # Divide into subproblems
-    min_distance_left = minimum_distance(s1_x, s1_y)
-    min_distance_right = minimum_distance(s2_x, s2_y)
+    min_distance_left = minimum_distance_too_slow(s1_x, s1_y)
+    min_distance_right = minimum_distance_too_slow(s2_x, s2_y)
     min_distance = min(min_distance_left, min_distance_right)
 
     # Check min distance for points that are in either set
@@ -81,6 +81,62 @@ def minimum_distance(x, y):
                 min_distance = dist
 
     return min_distance
+
+
+def minimum_distance_sorted(x, y):
+    # Handle base cases for small groups of points
+    if len(x) == 1:
+        return sys.maxsize
+    if len(x) == 2:
+        return compute_distance(x[0][0], x[1][0], x[0][1], x[1][1])
+    elif len(x) == 3:
+        return min(
+            compute_distance(x[0][0], x[1][0], x[0][1], x[1][1]),
+            compute_distance(x[0][0], x[2][0], x[0][1], x[2][1]),
+            compute_distance(x[1][0], x[2][0], x[1][1], x[2][1]),
+        )
+
+    # Get x coordinate that splits data into two equal sets
+    mid = len(x) // 2
+    mid_x = x[mid][0]
+
+    # Get corresponding y coordinates
+    s1_y, s2_y = [], []
+    for x_i, y_i in y:
+        if x_i <= mid_x:
+            s1_y.append((x_i, y_i))
+        else:
+            s2_y.append((x_i, y_i))
+
+    # Divide into subproblems
+    min_distance_left = minimum_distance_sorted(x[:mid], s1_y)
+    min_distance_right = minimum_distance_sorted(x[mid:], s2_y)
+    min_distance = min(min_distance_left, min_distance_right)
+
+    # Check min distance for points that are in either set
+    filtered_points_in_strip = []
+    for x_i, y_i in y:
+        if math.fabs(x_i - mid_x) < min_distance:
+            filtered_points_in_strip.append((x_i, y_i))
+
+    for i in range(0, len(filtered_points_in_strip)):
+        for j in range(i + 1, min(len(filtered_points_in_strip), i + 7)):
+            #print(f"i = {i}, j = {j}")
+            x2, y2 = filtered_points_in_strip[j]
+            x1, y1 = filtered_points_in_strip[i]
+            dist = compute_distance(x1, x2, y1, y2)
+            if dist < min_distance:
+                min_distance = dist
+
+    return min_distance
+
+
+def minimum_distance(x, y):
+    points = list(zip(x, y))
+    sorted_x_points = sorted(points, key=lambda tup: tup[0])
+    sorted_y_points = sorted(points, key=lambda tup: tup[1])
+
+    return minimum_distance_sorted(sorted_x_points, sorted_y_points)
 
 
 if __name__ == '__main__':
