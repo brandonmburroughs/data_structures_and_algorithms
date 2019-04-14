bd0f57698198b7d978d776e0da62d3e96419d1d7
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2019-04-14 16:13:52 -0700

    Adding solution

--- 0_algorithmic_toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ 0_algorithmic_toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,10 +1,35 @@
 #Uses python3
-
 import sys
 
+
+def matrix_max(matrix):
+    """Return the max element in a 2x2 matrix."""
+    return max([max(row) for row in matrix])
+
+
+def subset_matrix(matrix, i, j):
+    """Return the matrix of size (i, j) starting at (0, 0) in matrix."""
+    return [row[:j] for row in matrix[:i]]
+
+
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+    n = len(a)
+    m = len(b)
+
+    longest_common_subsequence_lengths = []
+    for i in range(n + 1):
+        row = []
+        for j in range(m + 1):
+            row.append(0)
+        longest_common_subsequence_lengths.append(row)
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if a[i - 1] == b[j - 1]:
+                longest_common_subsequence_lengths[i][j] = matrix_max(subset_matrix(longest_common_subsequence_lengths, i, j)) + 1
+
+    return matrix_max(longest_common_subsequence_lengths)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
