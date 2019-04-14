942b85de550ec27242367deb58692264e17f5229
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2019-04-14 13:45:03 -0700

    Adding solution

--- 0_algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ 0_algorithmic_toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,10 +1,22 @@
 # Uses python3
 import sys
 
-def get_change(m):
-    #write your code here
-    return m // 4
+
+def get_change(money):
+    coins = [1, 3, 4]
+    min_num_coins = [0] + [sys.maxsize] * money
+
+    for m in range(1, money + 1):
+        for coin in coins:
+            if m >= coin:
+                num_coins = min_num_coins[m - coin] + 1
+
+                if num_coins < min_num_coins[m]:
+                    min_num_coins[m] = num_coins
+
+    return min_num_coins[money]
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    money = int(sys.stdin.read())
+    print(get_change(money))
