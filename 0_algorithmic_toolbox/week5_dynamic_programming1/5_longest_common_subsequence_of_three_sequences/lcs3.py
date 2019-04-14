2f8be0653530c0f4aa07ccd56e671778f7a2e3aa
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2019-04-14 16:53:15 -0700

    Adding solution

--- 0_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ 0_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -1,10 +1,39 @@
 #Uses python3
-
 import sys
 
+
+def matrix_max(matrix):
+    """Return the max element in a 2x2 matrix."""
+    return max([max(row2) for row1 in matrix for row2 in row1])
+
+
+def subset_matrix(matrix, i, j, k):
+    """Return the matrix of size (i, j) starting at (0, 0) in matrix."""
+    return [[row2[:k] for row2 in row1[:j]] for row1 in matrix[:i]]
+
+
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    n = len(a)
+    m = len(b)
+    l = len(c)
+
+    longest_common_subsequence_length = []
+    for i in range(n + 1):
+        row1 = []
+        for j in range(m + 1):
+            row2 = []
+            for k in range(l + 1):
+                row2.append(0)
+            row1.append(row2)
+        longest_common_subsequence_length.append(row1)
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            for k in range(1, l + 1):
+                if a[i - 1] == b[j - 1] == c[k - 1]:
+                    longest_common_subsequence_length[i][j][k] = matrix_max(subset_matrix(longest_common_subsequence_length, i, j, k)) + 1
+
+    return matrix_max(longest_common_subsequence_length)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
