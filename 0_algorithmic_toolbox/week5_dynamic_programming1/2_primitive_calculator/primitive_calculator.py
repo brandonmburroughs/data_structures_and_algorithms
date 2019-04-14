ef31941fb0ef7ba4441c998de799ca8ffafb3d74
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2019-04-14 14:17:35 -0700

    Adding solution

--- 0_algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ 0_algorithmic_toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,7 +1,8 @@
 # Uses python3
 import sys
 
-def optimal_sequence(n):
+
+def optimal_sequence_wrong(n):
     sequence = []
     while n >= 1:
         sequence.append(n)
@@ -13,9 +14,30 @@ def optimal_sequence(n):
             n = n - 1
     return reversed(sequence)
 
-input = sys.stdin.read()
-n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
-for x in sequence:
-    print(x, end=' ')
+
+def optimal_sequence(n):
+    optimal_sequences = [[], [1]] # [list(range(1, i + 1)) for i in range(n + 1)]
+
+    for i in range(2, n + 1):
+        for previous_move in [i / 3, i / 2, i - 1]:
+            if int(previous_move) == previous_move:
+                previous_move = int(previous_move)
+
+                if len(optimal_sequences) < i + 1:
+                    optimal_sequences.append(optimal_sequences[previous_move] + [i])
+                elif len(optimal_sequences[previous_move]) + 1 < len(optimal_sequences[i]):
+                    optimal_sequences[i] = optimal_sequences[previous_move] + [i]
+                    break
+
+
+    return optimal_sequences[n]
+
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input)
+    sequence = list(optimal_sequence(n))
+    print(len(sequence) - 1)
+    for x in sequence:
+        print(x, end=' ')
