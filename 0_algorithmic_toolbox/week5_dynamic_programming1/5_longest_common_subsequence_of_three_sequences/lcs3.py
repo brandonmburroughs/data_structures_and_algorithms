24604b445f3bd63d60470ddfe7c474419042bdbb
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2019-04-14 17:14:06 -0700

    Changing to faster solution

--- 0_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ 0_algorithmic_toolbox/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -12,7 +12,7 @@ def subset_matrix(matrix, i, j, k):
     return [[row2[:k] for row2 in row1[:j]] for row1 in matrix[:i]]
 
 
-def lcs3(a, b, c):
+def lcs3_too_slow(a, b, c):  # Matrix max and matrix subset are too slow for when they are called often, e.g. identical arrays
     n = len(a)
     m = len(b)
     l = len(c)
@@ -34,6 +34,40 @@ def lcs3(a, b, c):
                     longest_common_subsequence_length[i][j][k] = matrix_max(subset_matrix(longest_common_subsequence_length, i, j, k)) + 1
 
     return matrix_max(longest_common_subsequence_length)
+
+
+def lcs3(a, b, c):
+    n = len(a)
+    m = len(b)
+    l = len(c)
+
+    longest_common_subsequence_length = []
+    for i in range(n + 1):
+        row1 = []
+        for j in range(m + 1):
+            row2 = []
+            for k in range(l + 1):
+                row2.append(0)
+            row1.append(row2)
+        longest_common_subsequence_length.append(row1)
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            for k in range(1, l + 1):
+                if a[i - 1] == b[j - 1] == c[k - 1]:
+                    longest_common_subsequence_length[i][j][k] = longest_common_subsequence_length[i - 1][j - 1][k - 1] + 1
+                else:
+                    longest_common_subsequence_length[i][j][k] = max(
+                        longest_common_subsequence_length[i - 1][j][k],
+                        longest_common_subsequence_length[i][j - 1][k],
+                        longest_common_subsequence_length[i][j][k - 1],
+                        longest_common_subsequence_length[i - 1][j - 1][k],
+                        longest_common_subsequence_length[i - 1][j][k - 1],
+                        longest_common_subsequence_length[i][j - 1][k - 1],
+                    )
+
+    return longest_common_subsequence_length[n][m][l]
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
